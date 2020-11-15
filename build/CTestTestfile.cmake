d8d6b5f6714cbd2061349c4e57f2da3ef8572108
Klausstaler <fredrik.klauss@gmx.de>
2020-11-15 18:36:39 +0100

    Add files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fred/ivr_assignment/src
+# Build directory: /home/fred/ivr_assignment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ivr_assignment")
---
+++ build/ivr_assignment/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fred/ivr_assignment/src/ivr_assignment
+# Build directory: /home/fred/ivr_assignment/build/ivr_assignment
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
