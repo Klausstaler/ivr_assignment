937736b107e1697704fb3a9f899d27f44af1a511
Klausstaler <fredrik.klauss@gmx.de>
2020-11-26 16:01:43 +0100

    Test link 1 estimation with GT data

--- src/image1.py
+++ src/image1.py
@@ -83,7 +83,9 @@ class image_converter:
     robot.link1.angle, robot.link2.angle, robot.link3.angle, robot.link4. angle = self.gt_angles
     gt_pos = robot.update_effector_estimate()
     #print(gt_pos, self.gt_angles[1:])
-    self._joint_angles = self.link1_estimator.links_cb(self._joint_angles, joint_locations_3d[3, :])
+    #self._joint_angles = self.link1_estimator.links_cb(self._joint_angles, joint_locations_3d[3, :])
+    print("GT location", gt_pos)
+    self._joint_angles = self.link1_estimator.links_cb(self.gt_angles[1:], gt_pos)
     message = Float64MultiArray()
     message.data = self._joint_angles
     self.joint_angles_pub.publish(message)
